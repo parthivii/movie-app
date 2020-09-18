b221d0f35cd0006ea041c065a237548add3755e6
parthivii <parthivisrivastava14@gmail.com>
2020-09-18 16:34:14 +0530

    Updates

---
+++ precache-manifest.3958a41cdade21f53fcacb5fcc6f0c45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30050d31967f07928764347ef814b45c",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "2de4e9f10706866554f0",
+    "url": "/movie-app/static/css/main.3af03026.chunk.css"
+  },
+  {
+    "revision": "03d71640163da01d0600",
+    "url": "/movie-app/static/js/2.0ca28602.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.0ca28602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2de4e9f10706866554f0",
+    "url": "/movie-app/static/js/main.df35c6e0.chunk.js"
+  },
+  {
+    "revision": "7e869a2352de7a9f3c65",
+    "url": "/movie-app/static/js/runtime-main.85ad0f9f.js"
+  }
+]);
